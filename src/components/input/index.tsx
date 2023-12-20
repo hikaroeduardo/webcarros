41c7a42634bb6292ba720a1d5011df81d0875940
hikaroeduardo <hikaroeduardobandeira@gmail.com>
2023-12-20 10:06:16 -0300

    feat: pagina home + config de react-hook-form e zod

---
+++ src/components/input/index.tsx
@@ -0,0 +1,34 @@
+import { RegisterOptions, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  type: string;
+  placeholder: string;
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  register: UseFormRegister<any>;
+  error?: string;
+  rules?: RegisterOptions;
+}
+
+export function Input({
+  type,
+  placeholder,
+  name,
+  register,
+  rules,
+  error,
+}: InputProps) {
+  return (
+    <div>
+      <input
+        className="w-full border-2 rounded-md h-11 px-2"
+        type={type}
+        placeholder={placeholder}
+        {...register(name, rules)}
+        id={name}
+      />
+
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,44 @@
+import { Container } from "../../components/container";
+
 export function Home() {
-    return (
-        <div>
-            <h1>Página Home</h1>
-        </div>
-    )
+  return (
+    <Container>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+          className="w-full border-w rounded-lg h-9 px-3 outline-none"
+          placeholder="Digite o nome do carro..."
+        />
+
+        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+          Buscar
+        </button>
+      </section>
+
+      <h1 className="font-bold text-center mt-6 text-2xl mb-4">
+        Carros novos e usados em todo o Brasil
+      </h1>
+
+      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <section className="w-full bg-white rounded-lg">
+          <img
+            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202312/20231206/chevrolet-onix-1.0-flex-lt-manual-wmimagem16020860616.jpg?s=fill&w=552&h=414&q=60"
+            alt="Carro"
+          />
+          <p className="font-bold mt-1 mb-2 px-2">CHEVROLET ONIX</p>
+
+          <div className="flex flex-col px-2">
+            <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 km</span>
+            <strong className="text-black font-medium text-xl">R$ 190.000</strong>
+          </div>
+
+          <div className="w-full h-px bg-slate-200 my-2"></div>
+
+          <div className="px-2 pb-2">
+            <span className="text-black">Teresina - PI</span>
+          </div>
+        </section>
+      </main>
+    </Container>
+  );
 }
