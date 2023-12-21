bc2266746eadd08760069d393f14d5b605068d88
hikaroeduardo <hikaroeduardobandeira@gmail.com>
2023-12-21 10:23:27 -0300

    feat: criado contexto de autenticacao

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import { router } from "./App.tsx";
+import AuthProvider from "./contexts/AuthContext.tsx";
 
 import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
