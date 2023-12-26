254d002bd2c6359655bf09a0ee457337ce2295e2
hikaroeduardo <hikaroeduardobandeira@gmail.com>
2023-12-26 10:12:21 -0300

    feat: Buscando carros do banco de dados e renderizando na pagina principal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,62 @@
+/* eslint-disable prefer-const */
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 import { Container } from "../../components/container";
 
+import { db } from "../../services/firebaseConnection";
+import { collection, query, orderBy, getDocs } from "firebase/firestore";
+
+type PriceProps = string | number;
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: PriceProps;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
 export function Home() {
+  const [cars, setCars] = useState<CarProps[]>();
+
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        let listcars = [] as CarProps[];
+
+        snapshot.forEach((doc) => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          });
+        });
+
+        setCars(listcars);
+      });
+    }
+
+    loadCars();
+  }, []);
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -19,25 +75,33 @@ export function Home() {
       </h1>
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202312/20231206/chevrolet-onix-1.0-flex-lt-manual-wmimagem16020860616.jpg?s=fill&w=552&h=414&q=60"
-            alt="Carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">CHEVROLET ONIX</p>
+        {cars?.map((car) => (
+          <Link key={car?.id} to={`/car/${car?.id}`}>
+            <section className="w-full bg-white rounded-lg">
+              <img
+                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                src={car?.images[0].url}
+                alt=""
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car?.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 km</span>
-            <strong className="text-black font-medium text-xl">R$ 190.000</strong>
-          </div>
+              <div className="flex flex-col px-2">
+                <span className="text-zinc-700 mb-6">
+                  Ano {car?.year} | {car?.km} km
+                </span>
+                <strong className="text-black font-medium text-xl">
+                  R$ {car?.price}
+                </strong>
+              </div>
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
+              <div className="w-full h-px bg-slate-200 my-2"></div>
 
-          <div className="px-2 pb-2">
-            <span className="text-black">Teresina - PI</span>
-          </div>
-        </section>
+              <div className="px-2 pb-2">
+                <span className="text-black">{car?.city}</span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );
