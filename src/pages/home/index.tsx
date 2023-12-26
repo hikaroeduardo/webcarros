b5cb3375b4e4b4c92c27b8e56454d0c9599ab93a
hikaroeduardo <hikaroeduardobandeira@gmail.com>
2023-12-26 10:26:32 -0300

    fix: Resolvendo problema de Layout shift

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -28,6 +28,7 @@ interface CarProps {
 
 export function Home() {
   const [cars, setCars] = useState<CarProps[]>();
+  const [loadImages, setLoadImages] = useState<string[]>([]);
 
   useEffect(() => {
     function loadCars() {
@@ -57,6 +58,10 @@ export function Home() {
     loadCars();
   }, []);
 
+  function handleImageLoad(id: string) {
+    setLoadImages((prevImageLoaded) => [...prevImageLoaded, id]);
+  }
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -78,10 +83,20 @@ export function Home() {
         {cars?.map((car) => (
           <Link key={car?.id} to={`/car/${car?.id}`}>
             <section className="w-full bg-white rounded-lg">
+              <div
+                className="w-full h-72 rounded-lg bg-slate-200"
+                style={{
+                  display: loadImages.includes(car?.id) ? "none" : "block",
+                }}
+              ></div>
               <img
                 className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
                 src={car?.images[0].url}
                 alt=""
+                onLoad={() => handleImageLoad(car?.id)}
+                style={{
+                  display: loadImages.includes(car?.id) ? "block" : "none",
+                }}
               />
               <p className="font-bold mt-1 mb-2 px-2">{car?.name}</p>
 
